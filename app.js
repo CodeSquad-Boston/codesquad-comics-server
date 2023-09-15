92c5e6cf81c48dca45bff3a401205fe432432c6b
kitamreik <kitdamreik@gmail.com>
2023-09-14 21:43:04 -0600

    CCS-1 done

--- app.js
+++ app.js
@@ -4,46 +4,40 @@ const path = require('node:path');
 const app = express();
 const PORT = 3000;
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname + '/views'));
-
 app.use(morgan('dev'));
 
 app.use(express.json());
 //configure the public directory (is having the slash pertinent)
 app.use(express.static(path.join(__dirname + '/public')));
 
-// Routes
+// Create five basic GET routes with the following information
+app.get('/', (request, response, next) => {
+  //response.send("This is the index/home page");
+ // CHANGE the handler from .send to status().json with a success message 
+ response.status(200).json({success: {message: "Index successful"}, statusCode: 200});
 
-app.get('/', (request, response) => {
-  response.send("This is the index/home page")
-  //response.send({success: {message: "Index successful"}, statusCode: 200})
-  response.status(200).json({success: {message: "Index successful"}, statusCode: 200})
 });
 
 app.get('/about', (request, response) => {
-  response.send("This is the about page");
+  //response.send("This is the about page");
+  response.status(200).json({success: {message: "About successful"}, statusCode: 200});
 });
 
 app.get('/login', (request, response) => {
-  response.send("This is the login page");
+  //response.send("This is the login page");
+  response.status(200).json({success: {message: "Login successful"}, statusCode: 200})
 });
 
 app.get('/admin-console', (request, response) => {
-  response.send("This is the admin console page");
+  //response.send("This is the admin console page");
+  response.status(200).json({success: {message: "Admin console successful"}, statusCode: 200})
 });
 
 app.get('/admin-console/create-book', (request, response) => {
-  response.send("This is the create book page");
+  //response.send("This is the create book page")
+  response.status(200).json({success: {message: "Create book successful"}, statusCode: 200});
 });
 
-//changing routes to allow for API/postman
-app.get('/api/books', (request, response) => {
-  response.send("This is for all of the books");
-})
-app.get('/api/books/:id', (request, response) => {
-  response.send("This is to find a specific book by the ID");
-})
 
 //Server
 app.listen(PORT, () => {
