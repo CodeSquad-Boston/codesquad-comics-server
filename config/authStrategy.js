4e733fa1ca753a88524537668c42f3a22d20bbce
kitamreik <kitdamreik@gmail.com>
2023-11-02 21:06:16 -0600

    MVP authStrategy.js/CCS-7

---
+++ config/authStrategy.js
@@ -0,0 +1,52 @@
+//require passport and bcrypt
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+//define the local strategy
+const LocalStrategy = require("passport-local").Strategy;
+//Test: define the github strategy
+
+//See if it works - google strategy
+
+//define the User model
+const User = require('../models/userModel')
+
+//implement the local strategy
+passport.use( //container to use the local strategy
+    new LocalStrategy(function verify(username, password, done) {
+        //initialize a new LocalStrategy by defining a function called verify, which has three parameters - username, password, done.
+      User.findOne({ username: username }) //we will find the User by the findOne() method and target the username as the parameter. 
+        .then((user) => { //use the .then() method as another container with the user as the parameter
+          if (!user) { //create an if statement to detect if there is NO user using the logical NOT operator
+            return done(null, false, { message: "User not found." });
+            //Within the if statement, use the return keyword that has the done callback function that contains three parameters - null, false and a message object that says "User not found."
+          }
+          //After the if statement, we will have bcrypt use the .compare() method with three parameters, and then chain an arrow function as a container. The parameters are password, user.password and (error, result) 
+          bcrypt.compare(password, user.password, (error, result) => {
+            console.log("result", result); //console.log two parameters, a string that says result and the result parameter
+            //stage an if statement that is able to catch errors.
+            if (error) {
+              return done(error); //Use the return keyword that has the done callback function that contains the error parameter
+            }
+            return done(null, user); //Use the return keyword that has the done callback function that contains two parameters - null and user
+          });
+        })
+        //Now it’s time to stage the .catch() method as another container with the error as the parameter to handle errors.
+        .catch((error) => { 
+          console.log(`There was an error finding user from database: ${error}`); //console.log if there were any errors within a template literal using the error parameter.
+        });
+    })
+);
+
+//implement the github strategy
+
+//implement the google strategy
+
+//use Passport documentation to implement the serializeUser/deserializeUser functions 
+passport.serializeUser((user, done) => { //Tell passport to serializeUser. Make a function with two parameters - user and a callback function called done.
+    done(null, user); //call the callback function done with two parameters - null and user
+});
+
+passport.deserializeUser((user, done) => { //Tell passport to deserializeUser. Make a function with two parameters - user and a callback function called done.
+    done(null, user); //call the callback function done with two parameters - null and user
+});
+//end
