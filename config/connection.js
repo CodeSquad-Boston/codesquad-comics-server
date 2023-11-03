fe8271984871e35bef739990c300e5924dd0e341
kitamreik <kitdamreik@gmail.com>
2023-11-02 19:56:09 -0600

    refactored connection.js/CCS-7

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,15 @@
-//CCS-6 Addition
-//Open connection.js in VSCode and require Mongoose
+//CCS-7 Refactoring
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
 
+//MongoDB connection with env
+mongoose
+.connect(process.env.DB_URL)
+.then(() => console.log("Mongodb is connected"))
+.catch((err) => console.log(err));
+
+/*
 //MongoDB connection without env 
 //Use mongoose.connect() to make your connection to your local MongoDB server.
 main().catch(err => console.log(err));
@@ -11,3 +17,4 @@ main().catch(err => console.log(err));
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/CS-Comics-24')};
 //For the first parameter of the connect() method, put in the actual MongoDB connection string 
+*/
