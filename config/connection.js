6e7f6eb6dd75fe07e720fe89ef32c63435662348
kitamreik <kitdamreik@gmail.com>
2024-04-25 17:07:40 -0600

    reconfigure connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,5 +9,7 @@ mongoose.set('strictQuery', false);
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/CS-Comics-24')};
+  await mongoose.connect('mongodb://localhost:27017/CS-Comics-24')}; //this is a link so the DB works on Kit's MongoDB.
 //For the first parameter of the connect() method, put in the actual MongoDB connection string 
+
+//Kit edit: students should have await mongoose.connect('') so their URLs aren't pushed to github
